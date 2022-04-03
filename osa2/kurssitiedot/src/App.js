0736cbe4b3c1d1de945b32cd4ab458f9704bbd67
linre-90 <juho.lindemark@gmail.com>
2022-04-03 12:12:15 +0300

    step9 complete

--- osa2/kurssitiedot/src/App.js
+++ osa2/kurssitiedot/src/App.js
@@ -62,9 +62,9 @@ const Total = (props) => {
 };
 
 /**
- * renders course objects
+ * renders single course object
  * @param {*} course - object
- * @returns
+ * @returns single course react component
  */
 const Course = ({ course }) => {
     return (
@@ -77,36 +77,56 @@ const Course = ({ course }) => {
 };
 
 const App = () => {
-    const course = {
-        name: "Half Stack application development",
-        id: 1,
-        parts: [
-            {
-                name: "Fundamentals of React",
-                exercises: 10,
-                id: 1,
-            },
-            {
-                name: "Using props to pass data",
-                exercises: 7,
-                id: 2,
-            },
-            {
-                name: "State of a component",
-                exercises: 14,
-                id: 3,
-            },
-            {
-                name: "Redux",
-                exercises: 11,
-                id: 4,
-            },
-        ],
-    };
+    const courses = [
+        {
+            name: "Half Stack application development",
+            id: 1,
+            parts: [
+                {
+                    name: "Fundamentals of React",
+                    exercises: 10,
+                    id: 1,
+                },
+                {
+                    name: "Using props to pass data",
+                    exercises: 7,
+                    id: 2,
+                },
+                {
+                    name: "State of a component",
+                    exercises: 14,
+                    id: 3,
+                },
+                {
+                    name: "Redux",
+                    exercises: 11,
+                    id: 4,
+                },
+            ],
+        },
+        {
+            name: "Node.js",
+            id: 2,
+            parts: [
+                {
+                    name: "Routing",
+                    exercises: 3,
+                    id: 1,
+                },
+                {
+                    name: "Middlewares",
+                    exercises: 7,
+                    id: 2,
+                },
+            ],
+        },
+    ];
 
     return (
         <div>
-            <Course course={course}></Course>
+            {courses.map((course) => (
+                <Course key={course.id} course={course}></Course>
+            ))}
         </div>
     );
 };
