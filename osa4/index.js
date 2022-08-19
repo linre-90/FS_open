71e289a2b55aaacffded7f442ba9b09a067a9a27
Juho <juho.lindemark@gmail.com>
2022-08-19 09:11:52 +0300

    sperated controller

--- osa4/index.js
+++ osa4/index.js
@@ -1,10 +1,10 @@
-const http = require("http");
+//const http = require("http");
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
 const config = require("./utils/config");
-const Blog = require("./models/blog.js");
+const blogRouter = require("./controllers/blogs.js");
 
 
 const mongoUrl = config.MONGODB_URI;
@@ -12,24 +12,8 @@ mongoose.connect(mongoUrl);
 
 app.use(cors());
 app.use(express.json());
+app.use('/api/blogs', blogRouter);
 
-app.get("/api/blogs", (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    });
-});
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body)
-  console.log(request.body);
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    });
-});
 
 const PORT = config.PORT;
 app.listen(PORT, () => {
