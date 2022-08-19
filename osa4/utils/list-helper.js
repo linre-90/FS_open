4abd046c19dbc98b3823e4f85327fc0f2289dab9
Juho <juho.lindemark@gmail.com>
2022-08-19 11:24:05 +0300

    added mostBlogs, mostLikes and test

--- osa4/utils/list-helper.js
+++ osa4/utils/list-helper.js
@@ -17,9 +17,65 @@ const totalLikes = (blogs) => {
 const favoriteBlog = (blogs) => {
     return [...blogs].sort((a, b) => a.likes - b.likes).reverse()[0];
 }
+
   
+const mostBlogs = (blogs) => {
+    const listCopy = sortByAuthor(blogs);
+
+    // count postings.
+    let prevAuthor = null;
+    let authorID = -1;
+    let perAuthorPost = [];
+
+    listCopy.forEach(blogPost => {
+        if(prevAuthor == null || blogPost.author.toLowerCase() != prevAuthor){
+            prevAuthor = blogPost.author.toLowerCase();
+            authorID++;
+            perAuthorPost[authorID] = {author: blogPost.author, blogs: 0};
+        }
+        perAuthorPost[authorID].blogs += 1;
+    });
+
+    return perAuthorPost.sort((a, b) => a.blogs - b.blogs).reverse()[0];
+}
+
+const mostLikes = (blogs) => {
+    const listCopy = sortByAuthor(blogs);
+
+    // count likes.
+    let prevAuthor = null;
+    let authorID = -1;
+    let perAuthorPost = [];
+
+    listCopy.forEach(blogPost => {
+        if(prevAuthor == null || blogPost.author.toLowerCase() != prevAuthor){
+            prevAuthor = blogPost.author.toLowerCase();
+            authorID++;
+            perAuthorPost[authorID] = {author: blogPost.author, likes: 0};
+        }
+        perAuthorPost[authorID].likes += blogPost.likes;
+    });
+
+    return perAuthorPost.sort((a, b) => a.likes - b.likes).reverse()[0];
+}
+
+const sortByAuthor = (blogs) => {
+    // sort list to author aplphabetical order
+    return [...blogs].sort((a, b) => {
+        if(a.author.toLowerCase() < b.author.toLowerCase()){
+            return -1;
+        }else if(a.author.toLowerCase() > b.author.toLowerCase()){
+            return 1;
+        }else{
+            return 0;
+        }
+    });
+}
+
 module.exports = {
     dummy,
     totalLikes,
-    favoriteBlog
+    favoriteBlog,
+    mostBlogs,
+    mostLikes
 }
