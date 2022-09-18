d8d7888f9d951c9ff688d7174d0fba13651cf37e
Juho <juho.lindemark@gmail.com>
2022-09-18 19:10:03 +0300

    4.18 complete

--- osa4/index.js
+++ osa4/index.js
@@ -6,6 +6,7 @@ const mongoose = require("mongoose");
 const config = require("./utils/config");
 const blogRouter = require("./controllers/blogs.js");
 const userRouter = require("./controllers/user.js");
+const loginRouter = require("./controllers/login");
 
 
 const mongoUrl = config.MONGODB_URI;
@@ -13,8 +14,9 @@ mongoose.connect(mongoUrl);
 
 app.use(cors());
 app.use(express.json());
-app.use('/api/blogs', blogRouter);
+app.use("/api/blogs", blogRouter);
 app.use("/api/users", userRouter);
+app.use("/api/login", loginRouter);
 
 
 const PORT = config.PORT;
